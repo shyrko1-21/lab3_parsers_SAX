2ba503ad65706b1fe61d09a321f1c6e6840ead9a
Shyrko1-21 <shyrko1.21@gmail.com>
2022-05-07 17:31:59 +0300

    commit

---
+++ src/sax/Main.java
@@ -0,0 +1,26 @@
+package sax;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) {
+        String path = new File("src\\books.xml").getAbsolutePath();
+        try {
+            SAXParserFactory factory = SAXParserFactory.newInstance();
+            SAXParser parser = factory.newSAXParser();
+
+            BookXMLHandler handler = new BookXMLHandler();
+            parser.parse(new File(path), handler);
+        } catch (IOException x) {
+            System.err.println(x);
+        } catch (ParserConfigurationException e) {
+            e.printStackTrace();
+        } catch (SAXException e) {
+            e.printStackTrace();
+        }
+    }
+}
